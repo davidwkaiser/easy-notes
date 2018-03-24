59ac65d3b399f6551df4e9776281cc2a4fe496ae
davidwkaiser <davidwkaiser@yahoo.com>
2018-03-23 21:41:11 -0500

    add imports to Note class

--- src/model/Note.java
+++ src/model/Note.java
@@ -1,4 +1,13 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+
 public class Note {
 }
