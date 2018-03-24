387ebbde9c8295ead3674036855e2146a4597c6a
davidwkaiser <davidwkaiser@yahoo.com>
2018-03-23 21:50:53 -0500

    add config

--- src/model/Note.java
+++ src/model/Note.java
@@ -1,12 +1,12 @@
 package model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.util.Date;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import org.springframework.data.annotation.CreatedDate;
+//import org.springframework.data.annotation.LastModifiedDate;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//import javax.persistence.*;
+//import javax.validation.constraints.NotBlank;
+//import java.util.Date;
 
 
 public class Note {
