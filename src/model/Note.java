f4057d59d7f62b0f4b1fefd5d6019979cf930ef9
davidwkaiser <davidwkaiser@yahoo.com>
2018-04-22 22:35:23 -0500

    start note model

--- src/model/Note.java
+++ src/model/Note.java
@@ -1,13 +1,19 @@
 package model;
 
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.annotation.LastModifiedDate;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//import javax.persistence.*;
-//import javax.validation.constraints.NotBlank;
-//import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+@EntityListenens(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class Note implements Serializable {
 
 
-public class Note {
 }
